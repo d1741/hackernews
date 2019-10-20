07e29b80d7742230af79d5018565769a201bf7fb
d1741 <traciecodes@gmail.com>
2019-10-20 15:43:15 -0500

    Added mapTime component to return the age of articles

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,6 +6,7 @@ import {
   StoryMeta,
   StoryMetaElement
 } from "../styles/StoryStyles";
+import { mapTime } from "../mappers/mapTime";
 
 export const Story = ({ storyId }) => {
   const [story, setStory] = useState({});
@@ -24,8 +25,8 @@ export const Story = ({ storyId }) => {
           <StoryMetaElement color="#000">By:</StoryMetaElement> {story.by}
         </span>
         <span data-testid="story-time">
-          <StoryMetaElement color="#000">Posted:</StoryMetaElement>
-          {` ${story.time} `}
+          <StoryMetaElement color="#000">Posted: </StoryMetaElement>{" "}
+          {mapTime(story.time)}
         </span>
       </StoryMeta>
     </StoryWrapper>
