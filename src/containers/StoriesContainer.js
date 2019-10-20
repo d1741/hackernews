92a52b5745e1672647d259f9841db531ae5edeb0
d1741 <traciecodes@gmail.com>
2019-10-20 16:09:14 -0500

    Midway commit (58 min left) WIP

--- src/containers/StoriesContainer.js
+++ src/containers/StoriesContainer.js
@@ -5,7 +5,10 @@ import {
   GlobalStyle,
   StoriesContainerWrapper
 } from "../styles/StoriesContainerStyles";
+import { useInfiniteScroll } from "../hooks/useInfiniteScroll";
+
 export const StoriesContainer = () => {
+  const { count } = useInfiniteScroll;
   const [storyIds, setStoryIds] = useState([]);
 
   useEffect(() => {
@@ -17,7 +20,7 @@ export const StoriesContainer = () => {
       <GlobalStyle />
       <StoriesContainerWrapper data-test-id="stories-container">
         <h1>Hacker News</h1>
-        {storyIds.map(storyId => (
+        {storyIds.slice(0, count).map(storyId => (
           <Story key={storyId} storyId={storyId} />
         ))}
       </StoriesContainerWrapper>
